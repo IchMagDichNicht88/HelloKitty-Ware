89621c397237c2d388fe2a7ba9f7f26de99f213c
IchMagDichNicht88 <discordistgeil348@gmail.com>
2025-10-20 19:33:57 +0200

    Create Auto Search Game.lua

---
+++ Auto Search Game.lua
@@ -0,0 +1,12 @@
+local placeId = game.PlaceId
+
+-- The Revenant
+if placeId == 93531785397649 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/HelloKitty-Ware/refs/heads/main/THE%20REVENANT%20Sunrisen.lua"))()
+
+-- Blackout Revival
+elseif placeId == 137064773215574 or  placeId == 135739469505435 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/HelloKitty-Ware/refs/heads/main/Blackout%20Revival.lua"))()
+else
+    warn("No Script For PlaceId: " .. placeId)
+end
