97d8fc95aaa8b1bd486fda2df676a23110a13ee4
IchMagDichNicht88 <discordistgeil348@gmail.com>
2025-10-28 19:29:10 +0100

    Update HelloKittyWare.lua

--- HelloKittyWare.lua
+++ HelloKittyWare.lua
@@ -7,6 +7,10 @@ if placeId == 93531785397649 then
 -- Blackout Revival
 elseif placeId == 137064773215574 or  placeId == 135739469505435 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/HelloKitty-Ware/refs/heads/main/Blackout%20Revival.lua"))()
+
+-- Sound Space
+elseif placeId == 2677609345 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/HelloKitty-Ware/refs/heads/main/SoundSpace.lua"))()
 else
     warn("No Script For PlaceId: " .. placeId)
 end
